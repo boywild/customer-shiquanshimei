12d14306eed537001b50cf028697e0dd037f2dcd
chentian <chentian.ct@qq.com>
2019-02-24 23:17:03 +0800

    测试接口地址

--- src/views/userCenter/memberinfo/action.js
+++ src/views/userCenter/memberinfo/action.js
@@ -1,7 +1,14 @@
 import api from 'utils/api';
 import createAsyncAction from 'utils/createAsyncAction';
 
-const getOutlets = () => createAsyncAction('OUTLETS_GET', () => api.get('/outlets'));
+const getOutlets = () =>
+    createAsyncAction('OUTLETS_GET', () =>
+        api.post('/announce/query', {
+            content: '公告内容',
+            pageNum: 1,
+            pageSize: 10
+        })
+    );
 
 export default {
     getOutlets
